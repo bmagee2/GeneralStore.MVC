fd90bb5c94d3ffe25030d251793063bd2af5fe2f
bmagee2 <bamagee2@gmail.com>
2020-09-22 15:57:41 -0400

    create

---
+++ GeneralStore.MVC/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using GeneralStore.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralStore.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        // Add the application DB Context (link to the database)
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: Product -- list of all products
+        public ActionResult Index()
+        {
+            return View(_db.Products.ToList());     // returns Products table in db as a list 
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
